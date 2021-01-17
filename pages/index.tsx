436f3c41b029bc778e6a8f00e6217b6c4a5e0590
Jeroen Ooms <jooms@xebia.com>
2021-01-17 15:07:35 +0100

    Fix mobile view

--- pages/index.tsx
+++ pages/index.tsx
@@ -146,6 +146,16 @@ export const HomePage = ({ stores }: HomePageProps) => {
     spinner: true,
   });
 
+  const contentSpinnerClasses = cx({
+    contentSpinner: true,
+    "contentSpinner--spinning": locationLoading,
+  });
+
+  const storeBlocksClasses = cx({
+    storeBlocks: true,
+    "storeBlocks--hidden": locationLoading,
+  });
+
   return (
     <div className={styles.container}>
       <Head>
@@ -215,7 +225,23 @@ export const HomePage = ({ stores }: HomePageProps) => {
           </button>
         </div>
 
-        <div className={styles.storeBlocks}>
+        <div className={contentSpinnerClasses}>
+          <svg
+            className={spinnerClasses}
+            aria-hidden="true"
+            focusable="false"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <path
+              fill="currentColor"
+              d="M304 48c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-48 368c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm208-208c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zM96 256c0-26.51-21.49-48-48-48S0 229.49 0 256s21.49 48 48 48 48-21.49 48-48zm12.922 99.078c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.491-48-48-48zm294.156 0c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.49-48-48-48zM108.922 60.922c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.491-48-48-48z"
+            ></path>
+          </svg>
+        </div>
+
+        <div className={storeBlocksClasses}>
           {currentStores.map((store) => (
             <div className={styles.storeBlock} key={store.id}>
               <img
