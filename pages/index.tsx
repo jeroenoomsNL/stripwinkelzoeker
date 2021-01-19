274a2ddc7ca8c4582bc56393f20b7104a2293163
Jeroen Ooms <jooms@xebia.com>
2021-01-19 08:42:43 +0100

    Fix getLocation issues, add social image and illustration

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,13 @@ export const HomePage = ({ stores }: HomePageProps) => {
   const [showDelivery, setDelivery] = useState(false);
   const [showPickup, setPickup] = useState(false);
   const [locationLoading, setLocationLoading] = useState(false);
+  const [currentLocation, setCurrentLocation] = useState({});
   const seachForm = useRef(null);
+
+  useEffect(() => {
+    const loc: any = currentLocation;
+    getStoresByLocation(loc.latitude, loc.longitude);
+  }, [currentLocation]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -59,9 +65,8 @@ export const HomePage = ({ stores }: HomePageProps) => {
 
     if (navigator?.geolocation) {
       setLocationLoading(true);
-      setStores(stores);
-      setQuery("");
       seachForm.current.reset();
+      setQuery("");
 
       navigator.geolocation.getCurrentPosition(
         getLocationSuccess,
@@ -73,21 +78,20 @@ export const HomePage = ({ stores }: HomePageProps) => {
   useEffect(() => {
     window.scrollTo(0, 0);
     if (useCurrentLocation) return;
-
-    doFuzzySearch(currentQuery, [...stores]);
+    doFuzzySearch(currentQuery, stores);
   }, [useCurrentLocation, showDelivery, showPickup]);
 
   function getLocationSuccess(position: any) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
+    setCurrentLocation({ latitude, longitude });
     setLocationLoading(false);
-    setUseCurrentLocation(true);
-    getStoresByLocation(latitude, longitude);
   }
 
   function getLocationError() {
     setLocationLoading(false);
     setUseCurrentLocation(false);
+    doFuzzySearch("", stores);
     console.log("Unable to retrieve location");
   }
 
@@ -113,7 +117,7 @@ export const HomePage = ({ stores }: HomePageProps) => {
       }
     });
 
-    doFuzzySearch(currentQuery, [...newStores]);
+    doFuzzySearch(currentQuery, [...newStores], 12);
   };
 
   const resetFilters = () => {
@@ -140,10 +144,16 @@ export const HomePage = ({ stores }: HomePageProps) => {
     }
 
     setUseCurrentLocation(false);
-    doFuzzySearch(query, [...stores]);
+    doFuzzySearch(query, stores);
   };
 
-  const doFuzzySearch = (searchQuery: string, storesArray: Store[]) => {
+  const doFuzzySearch = (
+    searchQuery: string,
+    storesArray: Store[],
+    limit?: number
+  ) => {
+    let foundStores: any;
+
     if (searchQuery && searchQuery !== "") {
       let options: Fuse.IFuseOptions<Store> = {
         threshold: 0.3,
@@ -162,17 +172,20 @@ export const HomePage = ({ stores }: HomePageProps) => {
           (showPickup && store.doesPickup === false)
         );
       });
-      const newStores: any = fuse.search(searchQuery);
-
-      setStores(newStores.map((store: SearchItem) => store.item));
+      foundStores = fuse
+        .search(searchQuery)
+        .map((store: SearchItem) => store.item);
     } else {
-      const newStores: Store[] = storesArray.filter((store) => {
+      foundStores = storesArray.filter((store) => {
         if (showPickup && !store.doesPickup) return false;
         if (showDelivery && !store.doesDeliver) return false;
         return true;
       });
-      setStores(newStores);
     }
+
+    const newStores = limit ? foundStores.slice(0, limit) : foundStores;
+
+    setStores([...newStores]);
   };
 
   const Image = ({ store }: ImageProps) => {
@@ -247,13 +260,25 @@ export const HomePage = ({ stores }: HomePageProps) => {
     <div className={styles.container}>
       <Head>
         <title>
-          Stripwinkel zoeker - Winkels en webshops van alle stripspeciaalzaken
-          in Nederland #steunjeboekhandel
+          Stripwinkelzoeker.nl - Vind een stripspeciaalzaak bij jou in de buurt
+          #steunjeboekhandel
         </title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap"
           rel="stylesheet"
+        />
+        <meta
+          name="description"
+          content="Stripwinkelzoeker.nl brengt alle stripspeciaalzaken van Nederland in kaart en laat zien of de winkel bij jouw in de buurt ook thuisbezorgt."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Stripwinkelzoeker.nl" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.stripwinkelzoeker.nl" />
+        <meta
+          property="og:image"
+          content="https://stripwinkelzoeker.nl/stripwinkelzoeker-header.png"
         />
       </Head>
 
@@ -266,115 +291,127 @@ export const HomePage = ({ stores }: HomePageProps) => {
       </header>
 
       <main className={styles.main}>
-        <div className={styles.searchContainer}>
-          <form ref={seachForm} onSubmit={preventSubmit}>
-            <input
-              type="text"
-              placeholder="Zoek een stripwinkel"
-              className={styles.searchInput}
-              onChange={handleSearch}
+        <div className={styles.searchGrid}>
+          <div className={styles.searchContainer}>
+            <form ref={seachForm} onSubmit={preventSubmit}>
+              <input
+                type="text"
+                placeholder="Zoek een stripwinkel"
+                className={styles.searchInput}
+                onChange={handleSearch}
+              />
+            </form>
+            <div className={styles.filterButtons}>
+              <button
+                type="button"
+                onClick={(e) => {
+                  e.currentTarget.blur();
+                  setUseCurrentLocation(!useCurrentLocation);
+                }}
+                className={locationButtonClasses}
+              >
+                <svg
+                  className={styles.icon}
+                  aria-hidden="true"
+                  focusable="false"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 384 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"
+                  ></path>
+                </svg>
+                <svg
+                  className={spinnerClasses}
+                  aria-hidden="true"
+                  focusable="false"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M304 48c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-48 368c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm208-208c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zM96 256c0-26.51-21.49-48-48-48S0 229.49 0 256s21.49 48 48 48 48-21.49 48-48zm12.922 99.078c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.491-48-48-48zm294.156 0c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.49-48-48-48zM108.922 60.922c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.491-48-48-48z"
+                  ></path>
+                </svg>{" "}
+                <span className={styles.buttonText}>
+                  <span className={styles.desktopText}>
+                    Winkels in de buurt
+                  </span>
+                  <span className={styles.mobileText} aria-hidden="true">
+                    In de buurt
+                  </span>
+                </span>
+              </button>
+              <button
+                type="button"
+                onClick={(e) => {
+                  e.currentTarget.blur();
+                  setDelivery(!showDelivery);
+                }}
+                className={deliveryButtonClasses}
+              >
+                <svg
+                  className={styles.icon}
+                  aria-hidden="true"
+                  focusable="false"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 640 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M624 352h-16V243.9c0-12.7-5.1-24.9-14.1-33.9L494 110.1c-9-9-21.2-14.1-33.9-14.1H416V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48v320c0 26.5 21.5 48 48 48h16c0 53 43 96 96 96s96-43 96-96h128c0 53 43 96 96 96s96-43 96-96h48c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zM160 464c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm320 0c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm80-208H416V144h44.1l99.9 99.9V256z"
+                  ></path>
+                </svg>
+                <span className={styles.buttonText}>
+                  <span className={styles.desktopText}>
+                    Winkels met bezorgservice
+                  </span>
+                  <span className={styles.mobileText} aria-hidden="true">
+                    Bezorgservice
+                  </span>
+                </span>
+              </button>
+              <button
+                type="button"
+                onClick={(e) => {
+                  e.currentTarget.blur();
+                  setPickup(!showPickup);
+                }}
+                className={pickupButtonClasses}
+              >
+                <svg
+                  className={styles.icon}
+                  aria-hidden="true"
+                  focusable="false"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 640 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M496 224c-79.6 0-144 64.4-144 144s64.4 144 144 144 144-64.4 144-144-64.4-144-144-144zm64 150.3c0 5.3-4.4 9.7-9.7 9.7h-60.6c-5.3 0-9.7-4.4-9.7-9.7v-76.6c0-5.3 4.4-9.7 9.7-9.7h12.6c5.3 0 9.7 4.4 9.7 9.7V352h38.3c5.3 0 9.7 4.4 9.7 9.7v12.6zM320 368c0-27.8 6.7-54.1 18.2-77.5-8-1.5-16.2-2.5-24.6-2.5h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h347.1c-45.3-31.9-75.1-84.5-75.1-144zm-96-112c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128z"
+                  ></path>
+                </svg>
+                <span className={styles.buttonText}>
+                  <span className={styles.desktopText}>
+                    Ophalen in de winkel
+                  </span>
+                  <span className={styles.mobileText} aria-hidden="true">
+                    Pickup
+                  </span>
+                </span>
+              </button>
+            </div>
+          </div>
+          <div className={styles.searchImageContainer}>
+            <img
+              src="/stripwinkelzoeker-illustratie.png"
+              className={styles.searchImage}
             />
-          </form>
-          <div className={styles.filterButtons}>
-            <button
-              type="button"
-              onClick={(e) => {
-                e.currentTarget.blur();
-                setUseCurrentLocation(!useCurrentLocation);
-              }}
-              className={locationButtonClasses}
-            >
-              <svg
-                className={styles.icon}
-                aria-hidden="true"
-                focusable="false"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 384 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"
-                ></path>
-              </svg>
-              <svg
-                className={spinnerClasses}
-                aria-hidden="true"
-                focusable="false"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M304 48c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-48 368c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm208-208c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zM96 256c0-26.51-21.49-48-48-48S0 229.49 0 256s21.49 48 48 48 48-21.49 48-48zm12.922 99.078c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.491-48-48-48zm294.156 0c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.49-48-48-48zM108.922 60.922c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.491-48-48-48z"
-                ></path>
-              </svg>{" "}
-              <span className={styles.buttonText}>
-                <span className={styles.desktopText}>Winkels in de buurt</span>
-                <span className={styles.mobileText} aria-hidden="true">
-                  In de buurt
-                </span>
-              </span>
-            </button>
-            <button
-              type="button"
-              onClick={(e) => {
-                e.currentTarget.blur();
-                setDelivery(!showDelivery);
-              }}
-              className={deliveryButtonClasses}
-            >
-              <svg
-                className={styles.icon}
-                aria-hidden="true"
-                focusable="false"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 640 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M624 352h-16V243.9c0-12.7-5.1-24.9-14.1-33.9L494 110.1c-9-9-21.2-14.1-33.9-14.1H416V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48v320c0 26.5 21.5 48 48 48h16c0 53 43 96 96 96s96-43 96-96h128c0 53 43 96 96 96s96-43 96-96h48c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zM160 464c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm320 0c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm80-208H416V144h44.1l99.9 99.9V256z"
-                ></path>
-              </svg>
-              <span className={styles.buttonText}>
-                <span className={styles.desktopText}>
-                  Winkels met bezorgservice
-                </span>
-                <span className={styles.mobileText} aria-hidden="true">
-                  Bezorgservice
-                </span>
-              </span>
-            </button>
-            <button
-              type="button"
-              onClick={(e) => {
-                e.currentTarget.blur();
-                setPickup(!showPickup);
-              }}
-              className={pickupButtonClasses}
-            >
-              <svg
-                className={styles.icon}
-                aria-hidden="true"
-                focusable="false"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 640 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M496 224c-79.6 0-144 64.4-144 144s64.4 144 144 144 144-64.4 144-144-64.4-144-144-144zm64 150.3c0 5.3-4.4 9.7-9.7 9.7h-60.6c-5.3 0-9.7-4.4-9.7-9.7v-76.6c0-5.3 4.4-9.7 9.7-9.7h12.6c5.3 0 9.7 4.4 9.7 9.7V352h38.3c5.3 0 9.7 4.4 9.7 9.7v12.6zM320 368c0-27.8 6.7-54.1 18.2-77.5-8-1.5-16.2-2.5-24.6-2.5h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h347.1c-45.3-31.9-75.1-84.5-75.1-144zm-96-112c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128z"
-                ></path>
-              </svg>
-              <span className={styles.buttonText}>
-                <span className={styles.desktopText}>Ophalen in de winkel</span>
-                <span className={styles.mobileText} aria-hidden="true">
-                  Pickup
-                </span>
-              </span>
-            </button>
           </div>
         </div>
 
@@ -527,9 +564,11 @@ export const HomePage = ({ stores }: HomePageProps) => {
           </div>
         )}
 
-        {currentQuery !== "" &&
-          !locationLoading &&
-          (useCurrentLocation || showDelivery || showPickup) && (
+        {!locationLoading &&
+          (useCurrentLocation ||
+            showDelivery ||
+            showPickup ||
+            currentQuery !== "") && (
             <div className={styles.buttonContainer}>
               <button
                 type="button"
@@ -542,22 +581,24 @@ export const HomePage = ({ stores }: HomePageProps) => {
           )}
 
         <div className={infoClasses}>
-          <h2>Waarom deze Stripwinkelzoeker?</h2>
           <div className={styles.infoGrid}>
-            <p className={styles.infoText}>
-              Stripwinkelzoeker is gemaakt om de stripspeciaalzaken met fysieke
-              winkels zichtbaarder te maken voor de stripliefhebber. Veel
-              winkels zijn op dit moment welliswaar gesloten maar bieden wel de
-              mogelijkheid om te bestellen via e-mail of een webshop. Door deze
-              winkels beter vindbaar te maken hopen we dat de stripliefhebbers
-              er sneller zulllen kopen.{" "}
-              <a href="https://www.hebban.nl/steunjeboekhandel">
-                #steunjeboekhandel
-              </a>
-            </p>
+            <div>
+              <h2>Waarom een Stripwinkelzoeker?</h2>
+              <p className={styles.infoText}>
+                Stripwinkelzoeker.nl is gemaakt om de stripspeciaalzaken met
+                fysieke winkels zichtbaarder te maken voor de stripliefhebber.
+                Veel winkels zijn op dit moment welliswaar gesloten maar bieden
+                wel de mogelijkheid om te bestellen via e-mail of een webshop.
+                Door deze winkels beter vindbaar te maken hopen we dat de
+                stripliefhebbers er sneller zulllen kopen.{" "}
+                <a href="https://www.hebban.nl/steunjeboekhandel">
+                  #steunjeboekhandel
+                </a>
+              </p>
+            </div>
             <div>
               <img
-                src="/steun-je-boekhandel.png"
+                src="/stripwinkelzoeker-illustratie.png"
                 className={styles.infoImage}
               />
             </div>
