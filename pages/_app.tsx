0a39f65bf9f2943edd79a494f378886f53f8af2a
Jeroen Ooms <info@jeroenooms.nl>
2022-02-17 08:44:20 +0100

    Add routing detection for Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,27 @@
 import Head from "next/head";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { pageview } from "../utils/gtag";
 import "../styles/global-styles.scss";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
