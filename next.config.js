4141344ba50575cf71418b1ce523212da8698787
Jeroen Ooms <info@jeroenooms.nl>
2023-03-18 10:33:09 +0100

    Fix redirects

--- next.config.js
+++ next.config.js
@@ -8,38 +8,4 @@ module.exports = {
   images: {
     domains: ["images.ctfassets.net"],
   },
-  async redirects() {
-    return [
-      {
-        source: "/winkel/stripwinkel-jopo-de-pojo",
-        destination: "/winkel/stripwinkel-jopo-de-pojo-haarlem",
-        permanent: true,
-      },
-      {
-        source: "/plaats/stripwinkels-in-den-haag",
-        destination: "/plaats/den-haag",
-        permanent: true,
-      },
-      {
-        source: "/winkel/stripwinkel-de-strip-aap",
-        destination: "/winkel/stripwinkel-de-strip-aap-enschede",
-        permanent: true,
-      },
-      {
-        source: "/winkel/stripwinkel-aelix-strips-en-comics",
-        destination: "/winkel/stripwinkel-aelix-strips-en-comics-den-haag",
-        permanent: true,
-      },
-      {
-        source: "/winkel/stripwinkel-dick-bos",
-        destination: "/winkel/stripwinkel-dick-bos-rotterdam",
-        permanent: true,
-      },
-      {
-        source: "/winkel/stripwinkel-bul-super",
-        destination: "/winkel/stripwinkel-bul-super-delft",
-        permanent: true,
-      },
-    ];
-  },
 };
