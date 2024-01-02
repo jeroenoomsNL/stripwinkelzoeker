85e7b43162bdb9a603b5d464c9d98b8622a35b22
Jeroen Ooms <info@jeroenooms.nl>
2024-01-02 11:04:12 +0100

    Implement new sitemap

--- next-sitemap.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_BASE_URL,
-  exclude: ["/sitemap.xml"], // <= exclude here
-  robotsTxtOptions: {
-    additionalSitemaps: [process.env.NEXT_PUBLIC_BASE_URL + "/sitemap.xml"],
-  },
-};
