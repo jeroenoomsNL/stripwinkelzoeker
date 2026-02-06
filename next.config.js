99058b8c379da696272ddb11ade3e0b00275730b
Jeroen Ooms <info@jeroenooms.nl>
2026-02-06 16:14:12 +0100

    Upgrade to Next 16

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 const path = require("path");
 
 module.exports = {
-  swcMinify: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
   images: {
-    domains: ["images.ctfassets.net"],
+    remotePatterns: [{ hostname: "images.ctfassets.net" }],
   },
   compiler: {
     styledComponents: true,
