4b76e34ac79c43ed9313777083f5bac6162c4aa3
Jeroen Ooms <info@jeroenooms.nl>
2026-07-29 19:09:46 +0200

    Update dependencies

--- getContentfulEnvironment.js
+++ getContentfulEnvironment.js
@@ -2,9 +2,12 @@ require("dotenv").config();
 const contentfulManagement = require("contentful-management");
 
 module.exports = function () {
-  const contentfulClient = contentfulManagement.createClient({
-    accessToken: process.env.CONTENTFUL_CONTENT_MANAGEMENT_ACCESS_TOKEN,
-  });
+  const contentfulClient = contentfulManagement.createClient(
+    {
+      accessToken: process.env.CONTENTFUL_CONTENT_MANAGEMENT_ACCESS_TOKEN,
+    },
+    { type: "legacy" },
+  );
 
   return contentfulClient
     .getSpace(process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID)
